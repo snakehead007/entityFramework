1adeffa55a0bb36646d05733ef03111ed85d67cd
snakehead007 <snakehead007@protonmail.com>
2020-01-19 17:49:03 +0100

    init

---
+++ Models/Orders.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramework
+{
+    public partial class Orders
+    {
+        public Orders()
+        {
+            OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int? EmployeeId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int? ShipVia { get; set; }
+        public decimal? Freight { get; set; }
+
+        public virtual Customers Customer { get; set; }
+        public virtual Employees Employee { get; set; }
+        public virtual Shippers ShipViaNavigation { get; set; }
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+
+
+namespace EntityFramework
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = new NorthwindContext();
+
+            do
+            {
+                switch (Menu())
+                {
+                    case 1:
+                        Overzicht(db);
+                        break;
+                    case 2:
+                        Analyse(db);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Deze optie is niet gekent");
+                        break;
+                }
+                Verder();
+            } while (true);
+        }
+
+        private static void Analyse(NorthwindContext db)
+        {
+            var regions =
+                from o in db.Orders
+                join c in db.Customers on o.CustomerId equals c.CustomerId
+                join d in db.OrderDetails on d.OrderId equals o.OrderId
+                group d.Quantity by c.Country
+                into groep
+                select new {Country = groep.Key, Aantal = groep.Sum()};
+                    
+            foreach (var region in regions)
+            {
+                Console.WriteLine("{0} {1}",region.Country, region.Aantal);
+            } 
+        }     
+
+        private static void Overzicht(NorthwindContext db)
+        {
+            var orders = from o in db.Orders join c in db.Customers on o.CustomerId equals c.CustomerId select new
+            {
+                o.OrderId,
+                o.CustomerId,
+                o.Customer,
+                o.Employee
+            };
+            int width = Console.WindowWidth;
+            int pad = width / 8;
+            Console.Write("Order ID".PadRight(pad)+"Klant ID".PadRight(pad)+"KlantNaam".PadRight(pad*4)+"Werknemer naam".PadRight(pad*2)+"\n");
+            Console.Write(String.Concat(Enumerable.Repeat("-", width)));
+            foreach (var order in orders)
+            {
+                Console.WriteLine(order.OrderId.ToString().PadRight(pad) +
+                                  order.CustomerId.PadRight(pad) + 
+                                  order.Customer.CompanyName.PadRight(pad * 4) +
+                                  order.Employee.FirstName.PadRight(pad * 2));
+            }
+            int orderId;
+            do
+            {
+                Console.Write("Geef een geldig order id in om detail info te bekijken: ");
+                
+                if (!int.TryParse(Console.ReadLine(), out orderId))
+                {
+                    Console.WriteLine("Onbekende input probeer opnieuw");
+                }
+            } while (orderId == 0);
+            Console.WriteLine("Product ID".PadRight(pad)+"Productnaam".PadRight(pad*3)+"Aantal".PadRight(pad)+"Eenheidsprijs".PadRight(pad*2)+"LijnWaarde".PadRight(pad));
+            Console.Write(String.Concat(Enumerable.Repeat("-", width)));
+            var products =
+                from o in db.OrderDetails
+                where o.OrderId == orderId
+                join p in db.Products
+                on o.Product.ProductId equals p.ProductId
+                select new
+            {
+                o.ProductId,
+                p.ProductName,
+                p.QuantityPerUnit,
+                o.UnitPrice,
+                p.ReorderLevel
+            };
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductId.ToString().PadRight(pad) +
+                                  product.ProductName.PadRight(pad) +
+                                  product.QuantityPerUnit.PadRight(pad * 3) +
+                                  product.UnitPrice.ToString().PadRight(pad * 2)+
+                                  product.ReorderLevel.ToString().PadRight(pad));
+            }
+        }
+        
+        
+
+        private static void Verder()
+        {
+            Console.Write("Druk op een toetst om verder te gaan");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        
+        private static int Menu()
+        {
+            string str = "1. Overzicht klantbestellingen\n2. Data analyse\n3. Stop\n > ";
+            Console.Write(str);
+            int keuze = 0;
+            int.TryParse(Console.ReadLine(), out keuze);
+            return keuze;
+        }
+    }
+}
